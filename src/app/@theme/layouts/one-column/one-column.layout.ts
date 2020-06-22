a9bc88193c5f388da11a052b39d4e687247ad165
Nurkumala Budi Fajrin <nurkumala_fajrin_24rpl@student.smktelkom-mlg.sch.id>
2020-06-22 18:08:20 +0700

    init project

--- src/app/@theme/layouts/one-column/one-column.layout.ts
+++ src/app/@theme/layouts/one-column/one-column.layout.ts
@@ -5,9 +5,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./one-column.layout.scss'],
   template: `
     <nb-layout windowMode>
-      <nb-layout-header fixed>
+      <!-- <nb-layout-header fixed>
         <ngx-header></ngx-header>
-      </nb-layout-header>
+      </nb-layout-header> -->
 
       <nb-sidebar class="menu-sidebar" tag="menu-sidebar" responsive>
         <ng-content select="nb-menu"></ng-content>
@@ -17,9 +17,9 @@ import { Component } from '@angular/core';
         <ng-content select="router-outlet"></ng-content>
       </nb-layout-column>
 
-      <nb-layout-footer fixed>
+      <!-- <nb-layout-footer fixed>
         <ngx-footer></ngx-footer>
-      </nb-layout-footer>
+      </nb-layout-footer> -->
     </nb-layout>
   `,
 })
